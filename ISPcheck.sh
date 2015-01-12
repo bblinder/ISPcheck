2ecf1fe86ad9e50eef8ae4cc45ce3518f242472b
bblinder <brandon.blinderman@gmail.com>
2015-01-12 12:25:34 -0500

    modified:   ISPcheck.sh

--- ISPcheck.sh
+++ ISPcheck.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 red='\033[0;31m'
+green='\033[0;32m'
 currentIP=$(curl -s icanhazip.com)
 
 if [[ $currentIP == 69.* ]]
 then
 	echo -e "${red}Towerstream"
+elif
+	[[ $currentIP -ne '69.*' ]]
+then
+	echo "${green}Time Warner"
 else
-	echo "Time Warner"
+	echo "Not connected to the internet :-("
 fi
